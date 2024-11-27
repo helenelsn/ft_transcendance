535a025b8ee0995d0fd040e76cfa84b67498958c
Helene Lesny <hlesny@student.42.fr>
2024-11-27 18:33:37 +0100

    garbage

---
+++ src/mysite/test_user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("users", views.all_user),
+    path("signin", views.sign_in),
+    path("<user>/", views.user_page),
+]
