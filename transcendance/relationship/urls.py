8e01bebb028689a02252dd25c3c557ac7f99bf4f
kalong <nomail@nomail.no>
2024-12-01 20:16:58 +0100

    on all user page, now display relion managment only to loggezd out users 	modified:   relationship/templates/relationship/all_user.html 	modified:   relationship/urls.py 	modified:   relationship/views.py

--- transcendance/relationship/urls.py
+++ transcendance/relationship/urls.py
@@ -8,7 +8,4 @@ urlpatterns = [
     path('friend_request/<username>', views.friend_request, name='friend_request'),
     path('delete_friend/<username>', views.delete_friend, name='delete_friend'),
     path('all_user/', views.all_user, name='all_user'),
-    # path('tournaments/', views.tournaments, name='tournaments'),
-    # path('create/', views.create_tournament, name='create_tournament'),
-    # path('<tournament_name>', views.show_tournament, name='show_tournament'),
 ]
--- transcendance/relationship/views.py
+++ transcendance/relationship/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from accounts.models import User
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 app_name = 'relationship'
 
@@ -13,9 +14,10 @@ def index(request):
 def all_user(request):
     return render(request, f'{app_name}/all_user.html', {'all_user' : User.objects.all()})
     
-
+@login_required
 def friend_request(request, username):
     return render(request, f'{app_name}/friend_request.html', {})
 
+@login_required
 def delete_friend(request, username):
     return render(request, f'{app_name}/index.html', {})
