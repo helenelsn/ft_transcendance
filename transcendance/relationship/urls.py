2fc5dc20251da27c378157b22f14532e5058a64a
kalong <nomail@nomail.no>
2024-12-01 18:45:07 +0100

    modified:   accounts/templates/accounts/profile_page.html 	new file:   relationship/templates/relationship/friend_request.html 	modified:   relationship/urls.py 	modified:   relationship/views.py 		introducing friend requests

--- transcendance/relationship/urls.py
+++ transcendance/relationship/urls.py
@@ -5,6 +5,7 @@ app_name = 'relationship'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('friend_request/', views.friend_request, name='friend_request'),
     # path('tournaments/', views.tournaments, name='tournaments'),
     # path('create/', views.create_tournament, name='create_tournament'),
     # path('<tournament_name>', views.show_tournament, name='show_tournament'),
--- transcendance/relationship/views.py
+++ transcendance/relationship/views.py
@@ -8,3 +8,6 @@ def index(request):
                "attribut" : "username",
                "show_url" : f"accounts:profile_page"}
     return render(request, f'{app_name}/index.html', context)
+
+def friend_request(request):
+    return render(request, f'{app_name}/friend_request.html', {})
