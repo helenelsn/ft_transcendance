1f62fcbd62cde89e5d23f3f096965df977a0f467
kalong <nomail@nomail.no>
2024-12-02 19:52:14 +0100

    end of templates/views refacto

--- transcendance/tournaments/views.py
+++ transcendance/tournaments/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Tournament
 from .forms import TournamentForm
+from common.utils import get_context, get_form_context, get_table_with_optional_action_context, get_table_context
 
 app_name = 'tournaments'
 
 def index(request):
-    context = {"objects" : Tournament.objects.all(),
-               "field" : "name",
-               "redir" : f"{app_name}:show_tournament"}
-    
+    context = get_table_context(
+        app_name=app_name,
+        objects=Tournament.objects.all(),
+        field='name',
+        url_to_redir=f"{app_name}:show_tournament",
+    )
     return render(request, f'{app_name}/index.html', context)
 
 
 
 def show_tournament(request, tournament_name):
     context = {"tournament" : get_object_or_404(Tournament,  name=tournament_name)}
-    return render(request, f'{app_name}/tournament.html', context)
+    return render(request, f'{app_name}/tournament.html', get_context(app_name, context))
 
 
 def create_tournament(request):
@@ -26,4 +29,4 @@ def create_tournament(request):
             return redirect(f"{app_name}:index")
     else:
         form = TournamentForm()
-    return render(request, f'{app_name}/create_tournament.html', {"forms": {form}})
+    return render(request, f'{app_name}/create_tournament.html', get_form_context(app_name, form))
